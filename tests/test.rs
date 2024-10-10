e6e23d46144e077cf435d7a76fb14f621ec475e0
sherbst-openai <sherbst@openai.com>
2024-10-10 10:41:21 -0700

    Handle combos of structs, enums, and multidimensional packed arrays as flat 1D buses (#23)

--- tests/test.rs
+++ tests/test.rs
@@ -376,44 +376,61 @@ endmodule
 
     #[test]
     fn test_export_interface_with_prefix() {
-        // Define ModuleB
-        let module_b_verilog = "
-        module ModuleB (
-            output [31:0] b_data,
-            output b_valid,
-            input b_ready
+        // Define ModuleA
+        let module_a_verilog = "
+        module ModuleA (
+            output [31:0] a_data,
+            output a_valid,
+            input a_ready
         );
         endmodule
         ";
 
-        let module_b = ModDef::from_verilog("ModuleB", module_b_verilog, true, false);
-        module_b.def_intf_from_prefix("b", "b_");
+        let module_a = ModDef::from_verilog("ModuleA", module_a_verilog, true, false);
+        module_a.def_intf_from_prefix("a", "a_");
 
-        let module_a = ModDef::new("ModuleA");
+        let module_b = ModDef::new("ModuleB");
+        module_b.instantiate(&module_a, None, None).get_intf("a").export_with_prefix("b", "b_");
 
-        let b_inst = module_a.instantiate(&module_b, Some("inst_b"), None);
-        let b_intf = b_inst.get_intf("b");
-        b_intf.export_with_prefix("a_");
+        let module_c = ModDef::new("ModuleC");
+        module_c.instantiate(&module_b, None, None).get_intf("b").export_with_prefix("c", "c_");
 
         assert_eq!(
-            module_a.emit(true),
+            module_c.emit(true),
             "\
-module ModuleA(
-  output wire [31:0] a_data,
-  output wire a_valid,
-  input wire a_ready
+module ModuleB(
+  output wire [31:0] b_data,
+  output wire b_valid,
+  input wire b_ready
 );
-  wire [31:0] inst_b_b_data;
-  wire inst_b_b_valid;
-  wire inst_b_b_ready;
-  ModuleB inst_b (
-    .b_data(inst_b_b_data),
-    .b_valid(inst_b_b_valid),
-    .b_ready(inst_b_b_ready)
+  wire [31:0] ModuleA_i_a_data;
+  wire ModuleA_i_a_valid;
+  wire ModuleA_i_a_ready;
+  ModuleA ModuleA_i (
+    .a_data(ModuleA_i_a_data),
+    .a_valid(ModuleA_i_a_valid),
+    .a_ready(ModuleA_i_a_ready)
   );
-  assign a_data[31:0] = inst_b_b_data[31:0];
-  assign a_valid = inst_b_b_valid;
-  assign inst_b_b_ready = a_ready;
+  assign b_data[31:0] = ModuleA_i_a_data[31:0];
+  assign b_valid = ModuleA_i_a_valid;
+  assign ModuleA_i_a_ready = b_ready;
+endmodule
+module ModuleC(
+  output wire [31:0] c_data,
+  output wire c_valid,
+  input wire c_ready
+);
+  wire [31:0] ModuleB_i_b_data;
+  wire ModuleB_i_b_valid;
+  wire ModuleB_i_b_ready;
+  ModuleB ModuleB_i (
+    .b_data(ModuleB_i_b_data),
+    .b_valid(ModuleB_i_b_valid),
+    .b_ready(ModuleB_i_b_ready)
+  );
+  assign c_data[31:0] = ModuleB_i_b_data[31:0];
+  assign c_valid = ModuleB_i_b_valid;
+  assign ModuleB_i_b_ready = c_ready;
 endmodule
 "
         );
@@ -484,7 +501,7 @@ endmodule
 
         wrapped_inst
             .get_intf("data_intf")
-            .export_with_prefix("top_");
+            .export_with_prefix("top", "top_");
 
         original_mod.set_usage(Usage::EmitNothingAndStop);
 
@@ -1150,6 +1167,113 @@ endmodule
             duration.as_secs() < 5,
             "Validation took too long: {:?}",
             duration
+        );
+    }
+
+    #[test]
+    fn test_subdivide() {
+        let a = ModDef::new("A");
+        a.add_port("out", IO::Output(8));
+        a.set_usage(Usage::EmitNothingAndStop);
+
+        let b = ModDef::new("B");
+        b.add_port("in", IO::Input(8));
+        b.set_usage(Usage::EmitNothingAndStop);
+
+        let top = ModDef::new("top");
+        let a = top.instantiate(&a, None, None).get_port("out");
+        let b = top.instantiate(&b, None, None).get_port("in");
+
+        for (asub, bsub) in a.subdivide(2).iter().zip(b.subdivide(2)) {
+            for (asubsub, bsubsub) in asub.subdivide(2).iter().zip(bsub.subdivide(2)) {
+                asubsub.connect(&bsubsub);
+            }
+        }
+
+        assert_eq!(
+            top.emit(true),
+            "\
+module top;
+  wire [7:0] A_i_out;
+  wire [7:0] B_i_in;
+  A A_i (
+    .out(A_i_out)
+  );
+  B B_i (
+    .in(B_i_in)
+  );
+  assign B_i_in[1:0] = A_i_out[1:0];
+  assign B_i_in[3:2] = A_i_out[3:2];
+  assign B_i_in[5:4] = A_i_out[5:4];
+  assign B_i_in[7:6] = A_i_out[7:6];
+endmodule
+"
+        );
+    }
+
+    #[test]
+    fn test_structs() {
+        let structs = "
+      package my_pack;
+        typedef struct packed {
+          logic [1:0] a; // width: 2
+          logic [2:0] b; // width: 3
+        } my_struct_t;
+      endpackage
+      ";
+        let module_a_verilog = "
+      module A (
+        output my_pack::my_struct_t [3:0] x // width: 20
+      );
+      endmodule";
+        let module_b_verilog = "
+      module B (
+        input my_pack::my_struct_t [1:0][1:0][1:0] y // width: 40
+      );
+      endmodule";
+        let a = ModDef::from_verilog(
+            "A",
+            format!("{structs}\n{module_a_verilog}").as_str(),
+            false,
+            false,
+        );
+        let b = ModDef::from_verilog(
+            "B",
+            format!("{structs}\n{module_b_verilog}").as_str(),
+            false,
+            false,
+        );
+
+        let top = ModDef::new("Top");
+        let a0 = top.instantiate(&a, Some("a0"), None);
+        let a1 = top.instantiate(&a, Some("a1"), None);
+        let b0 = top.instantiate(&b, Some("b0"), None);
+
+        let b0_inputs = b0.get_port("y").subdivide(2);
+
+        a0.get_port("x").connect(&b0_inputs[0]);
+        a1.get_port("x").connect(&b0_inputs[1]);
+
+        assert_eq!(
+            top.emit(true),
+            "\
+module Top;
+  wire [19:0] a0_x;
+  wire [19:0] a1_x;
+  wire [39:0] b0_y;
+  A a0 (
+    .x(a0_x)
+  );
+  A a1 (
+    .x(a1_x)
+  );
+  B b0 (
+    .y(b0_y)
+  );
+  assign b0_y[19:0] = a0_x[19:0];
+  assign b0_y[39:20] = a1_x[19:0];
+endmodule
+"
         );
     }
 }
